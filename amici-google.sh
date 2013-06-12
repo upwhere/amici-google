58377330c561881dd5cf7a3c8e22e792b695bf3a
root <root@command.SolidusServer.net>
2013-06-12 02:16:03 +0000

    first version of script

---
+++ amici-google.sh
@@ -0,0 +1,31 @@
+blocks="8.8.8.8 microsoft.com"
+
+for ns in $(
+	nslookup -q=TXT _spf.google.com 8.8.8.8 |
+	grep 'text\ =\ '|
+	sed '{s/ include:/\n/g;s/ ip4:/\n/g;s/ ip6:/\n/g}'|
+	awk '$2!="text"{print $1}'|
+	uniq );do
+		for range in $(
+			nslookup -q=TXT $ns 8.8.8.8|
+			grep 'text\ =\ '|
+			sed '{s/ ip4:/\n/g;s/ ip6:/\n/g}'|
+			awk '$2!="text"{print $1}'|
+			uniq );do
+				case $range in
+					*:*)
+						filter=ip6tables
+						oicmp=icmp6-adm-prohibited
+						iicmp=$oicmp
+					;;
+					*)
+						filter=iptables
+						oicmp=icmp-net-prohibited
+						iicmp=icmp-host-prohibited
+					;;
+				esac
+				reject="$filter -j REJECT --reject-with="
+				${reject}$oicmp -A OUTPUT -d $range
+				${reject}$iicmp -A INPUT -s $range
+		done
+done
